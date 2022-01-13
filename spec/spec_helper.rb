4428cd01475af70c4b304a660af04e551f7a513a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-13 17:02:02 -0600

    Add note in spec_helper for instructions to run SimpleCov report.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,5 @@ require 'date'
 require './lib/enigma'
 require './lib/shifts'
 require './lib/cipher'
+# To run the Simplecov report type the following into your terminal:
+# open coverage/index.html
