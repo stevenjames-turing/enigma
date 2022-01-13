03d53a05f54204bdd2fb821650fe02a29bde0f28
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-13 12:40:57 -0600

    Create a,b,c,d keys using 5-digit key passed as argument

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,6 +1,14 @@
 class Shifts
 
+  attr_reader :key, :date, :a_key, :b_key, :c_key, :d_key
+
   def initialize(key, date)
+    @key = key
+    @date = date
+    @a_key = @key[0..1]
+    @b_key = @key[1..2]
+    @c_key = @key[2..3]
+    @d_key = @key[3..4]
   end
 
 end
--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -3,10 +3,17 @@ require './lib/shifts'
 RSpec.describe Shifts do
 
   let (:shifts) {Shifts.new(key, date)}
+  let (:shifts_object) {Shifts.new("02715", "040895")}
 
   it 'exists' do
-    shifts_object = Shifts.new("key", "date")
     expect(shifts_object).to be_instance_of Shifts
+  end
+
+  it "uses the key passed as argument to create a,b,c,d keys" do
+    expect(shifts_object.a_key).to eq("02")
+    expect(shifts_object.b_key).to eq("27")
+    expect(shifts_object.c_key).to eq("71")
+    expect(shifts_object.d_key).to eq("15")
   end
 
 end
