26ddfe8906dd28d31583e4f98b42353eed63f4c4
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-13 12:50:34 -0600

    Create a,b,c,d offsets with date passed as argument

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,14 +1,20 @@
 class Shifts
 
-  attr_reader :key, :date, :a_key, :b_key, :c_key, :d_key
+  attr_reader :key, :date, :a_key, :b_key, :c_key, :d_key, :offset,
+              :a_offset, :b_offset, :c_offset, :d_offset
 
   def initialize(key, date)
     @key = key
     @date = date
-    @a_key = @key[0..1]
-    @b_key = @key[1..2]
-    @c_key = @key[2..3]
-    @d_key = @key[3..4]
+    @a_key = @key[0..1].to_i
+    @b_key = @key[1..2].to_i
+    @c_key = @key[2..3].to_i
+    @d_key = @key[3..4].to_i
+    @offset = ((@date.to_i * @date.to_i).to_s)[-4..-1]
+    @a_offset = @offset[-4].to_i
+    @b_offset = @offset[-3].to_i
+    @c_offset = @offset[-2].to_i
+    @d_offset = @offset[-1].to_i
   end
 
 end
--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -9,11 +9,19 @@ RSpec.describe Shifts do
     expect(shifts_object).to be_instance_of Shifts
   end
 
-  it "uses the key passed as argument to create a,b,c,d keys" do
-    expect(shifts_object.a_key).to eq("02")
-    expect(shifts_object.b_key).to eq("27")
-    expect(shifts_object.c_key).to eq("71")
-    expect(shifts_object.d_key).to eq("15")
+  it 'uses the key passed as argument to create a,b,c,d keys' do
+    expect(shifts_object.a_key).to eq(02)
+    expect(shifts_object.b_key).to eq(27)
+    expect(shifts_object.c_key).to eq(71)
+    expect(shifts_object.d_key).to eq(15)
+  end
+
+  it 'uses the date passed as argument to create a,b,c,d offsets' do
+    expect(shifts_object.offset).to eq("1025")
+    expect(shifts_object.a_offset).to eq(1)
+    expect(shifts_object.b_offset).to eq(0)
+    expect(shifts_object.c_offset).to eq(2)
+    expect(shifts_object.d_offset).to eq(5)
   end
 
 end
