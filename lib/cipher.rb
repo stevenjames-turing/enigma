df7c6f5eade9a8e8a75952d7d43e43dba6240635
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-13 12:18:52 -0600

    Add arguments of message, key, and date to Cipher#initialize

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,6 +1,11 @@
 class Cipher
 
-  def initialize
+  attr_reader :message, :key, :date
+
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
   end
 
 end
--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -2,9 +2,15 @@ require './lib/cipher'
 
 RSpec.describe Cipher do
 
-  let (:cipher) {Cipher.new}
+  let (:cipher) {Cipher.new("hello world", "02715", "040895")}
 
   it 'exists' do
     expect(cipher).to be_instance_of Cipher
+  end
+
+  it 'initializes with a message, key, and date' do
+    expect(cipher.message).to eq("hello world")
+    expect(cipher.key).to eq("02715")
+    expect(cipher.date).to eq("040895")
   end
 end
