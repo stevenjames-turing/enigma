a423575a4a331fa25003ecda204bd1eea5e2ffbc
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-13 16:02:34 -0600

    Add #encrypt to take in arguments and return hash with date, key, and encryption

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Enigma do
   it 'exists' do
     expect(enigma).to be_instance_of Enigma
   end
+
+  it 'can encrypt a message with a key and date' do
+    expected = {encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"}
+
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+
+
 end
