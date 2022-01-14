d11a570f584c87e697e6d4690a50dd5f7dcae09c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-14 11:37:43 -0600

    Add Cipher#decrypted_message_as_array to take decrypted message and break into arrays matching Shifts

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -78,4 +78,57 @@ class Cipher
     encrypted_array = encrypted_message_as_array
     encrypted_array[0].zip(encrypted_array[1]).zip(encrypted_array[2]).zip(encrypted_array[3]).join
   end
+
+  def decrypted_message_as_array
+    message_array = prep_message_for_shifts
+    a_letters = []; b_letters = []; c_letters = []; d_letters = []
+    decrypted_array = [a_letters, b_letters, c_letters, d_letters]
+    message_array[0].each do |letter|
+      if @character_set.include?(letter)
+        until @character_set[0] == letter
+          @character_set = @character_set.rotate
+        end
+        @character_set = @character_set.rotate(shifts.a_shift * -1)
+        a_letters << @character_set[0]
+      else
+        a_letters << letter
+      end
+    end
+    message_array[1].each do |letter|
+      if @character_set.include?(letter)
+        until @character_set[0] == letter
+          @character_set = @character_set.rotate
+        end
+        @character_set = @character_set.rotate(shifts.b_shift * -1)
+        b_letters << @character_set[0]
+      else
+        b_letters << letter
+      end
+    end
+    message_array[2].each do |letter|
+      if @character_set.include?(letter)
+        until @character_set[0] == letter
+          @character_set = @character_set.rotate
+        end
+        @character_set = @character_set.rotate(shifts.c_shift * -1)
+        c_letters << @character_set[0]
+      else
+        c_letters << letter
+      end
+    end
+    message_array[3].each do |letter|
+      if @character_set.include?(letter)
+        until @character_set[0] == letter
+          @character_set = @character_set.rotate
+        end
+        @character_set = @character_set.rotate(shifts.d_shift * -1)
+        d_letters << @character_set[0]
+      else
+        d_letters << letter
+      end
+    end
+    decrypted_array
+  end
+
+  
 end
