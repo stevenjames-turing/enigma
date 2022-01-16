14eb106f0246ec82d5b694363accc83e1631bb0d
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-16 16:24:53 -0600

    Refactor #encrypted_message_as_string and #decrypted_message as string into #message_as_string(type)

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -76,10 +76,23 @@ class Cipher
     encrypted_array
   end
 
-  def encrypted_message_as_string
-    encrypted_array = encrypted_message_as_array
-    encrypted_array[0].zip(encrypted_array[1]).zip(encrypted_array[2]).zip(encrypted_array[3]).join
+  def message_as_string(type)
+    if type == "encrypted"
+      return encrypted_message_as_array[0].zip(encrypted_message_as_array[1]).zip(encrypted_message_as_array[2]).zip(encrypted_message_as_array[3]).join
+    elsif type == "decrypted"
+      return decrypted_message_as_array[0].zip(decrypted_message_as_array[1]).zip(decrypted_message_as_array[2]).zip(decrypted_message_as_array[3]).join
+    end
   end
+
+  # def decrypted_message_as_string
+  #   decrypted_array = decrypted_message_as_array
+  #   decrypted_array[0].zip(decrypted_array[1]).zip(decrypted_array[2]).zip(decrypted_array[3]).join
+  # end
+
+  # def encrypted_message_as_string
+  #   encrypted_array = encrypted_message_as_array
+  #   encrypted_array[0].zip(encrypted_array[1]).zip(encrypted_array[2]).zip(encrypted_array[3]).join
+  # end
 
   def decrypted_message_as_array
     message_array = prep_message_for_shifts
@@ -132,10 +145,6 @@ class Cipher
     decrypted_array
   end
 
-  def decrypted_message_as_string
-    decrypted_array = decrypted_message_as_array
-    decrypted_array[0].zip(decrypted_array[1]).zip(decrypted_array[2]).zip(decrypted_array[3]).join
-  end
 
   def calculate_shifts
   # Offset is already known due to date being provided and the last 4 characters will always be `_end`.
@@ -143,49 +152,49 @@ class Cipher
   # we can use math to figure out the original key for the solution.
   # Take the shifts in increments of (shift + 27) and then subtract the offsets from those.
   # Using the result, compare the increments until a 5 digit key can be created using the overlaps
-    a,b,c,d = prep_message_for_shifts
-    character_shifts = {a_shift: a, b_shift: b, c_shift: c, d_shift: d}
+    # a,b,c,d = prep_message_for_shifts
+    # character_shifts = {a_shift: a, b_shift: b, c_shift: c, d_shift: d}
     starting_message = @message.split("")
-    last_4_of_message = @message.split("")[-4..-1]
+    last_4_of_message = starting_message[-4..-1]
     known_info = [" ","e","n","d"]
-    subtraction = 3
+    index_reducer = 3
     # CALCULATES THE SHIFTS USING THE "_END" AS KNOWN INFO
     until known_info.empty?
-      if (starting_message.length - subtraction) % 4 == 1
+      if (starting_message.length - index_reducer) % 4 == 1
         #starts with a_shift
         if @character_set.index(last_4_of_message.first) - @character_set.index(known_info.first) < 0
           @shifts.a_shift = ((@character_set.index(last_4_of_message.first) - @character_set.index(known_info.first)) + 27)
         else
           @shifts.a_shift = @character_set.index(last_4_of_message.first) - @character_set.index(known_info.first)
         end
-        last_4_of_message.shift && known_info.shift && subtraction -= 1
+        last_4_of_message.shift && known_info.shift && index_reducer -= 1
         # @shifts.a_offset
-      elsif (starting_message.length - subtraction) % 4 == 2
+      elsif (starting_message.length - index_reducer) % 4 == 2
         #starts with b_shift
         if @character_set.index(last_4_of_message.first) - @character_set.index(known_info.first) < 0
           @shifts.b_shift = ((@character_set.index(last_4_of_message.first) - @character_set.index(known_info.first)) + 27)
         else
           @shifts.b_shift = @character_set.index(last_4_of_message.first) - @character_set.index(known_info.first)
         end
-        last_4_of_message.shift && known_info.shift && subtraction -= 1
+        last_4_of_message.shift && known_info.shift && index_reducer -= 1
         # @shifts.b_offset
-      elsif (starting_message.length - subtraction) % 4 == 3
+      elsif (starting_message.length - index_reducer) % 4 == 3
         #starts with c_shift
         if @character_set.index(last_4_of_message.first) - @character_set.index(known_info.first) < 0
           @shifts.c_shift = ((@character_set.index(last_4_of_message.first) - @character_set.index(known_info.first)) + 27)
         else
           @shifts.c_shift = @character_set.index(last_4_of_message.first) - @character_set.index(known_info.first)
         end
-        last_4_of_message.shift && known_info.shift && subtraction -= 1
+        last_4_of_message.shift && known_info.shift && index_reducer -= 1
         # @shifts.c_offset
-      elsif (starting_message.length - subtraction) % 4 == 0
+      elsif (starting_message.length - index_reducer) % 4 == 0
         #starts with d_shift
         if @character_set.index(last_4_of_message.first) - @character_set.index(known_info.first) < 0
           @shifts.d_shift = ((@character_set.index(last_4_of_message.first) - @character_set.index(known_info.first)) + 27)
         else
           @shifts.d_shift = @character_set.index(last_4_of_message.first) - @character_set.index(known_info.first)
         end
-        last_4_of_message.shift && known_info.shift && subtraction -= 1
+        last_4_of_message.shift && known_info.shift && index_reducer -= 1
         # @shifts.d_offset
       end
     end
@@ -200,7 +209,7 @@ class Cipher
   def crack_encryption
     calculate_shifts
     @key = shifts.get_valid_key
-    decrypted_message_as_string
+    message_as_string("decrypted")
   end
 
 end
