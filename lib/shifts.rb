6607f6161d041245188b5dddfad68bcdfb0973a3
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-17 16:31:18 -0600

    Remove unused method #valid_key?

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,10 +1,8 @@
 class Shifts
 
   attr_reader :date, :offset, :a_offset, :b_offset, :c_offset, :d_offset
-
   attr_accessor :a_shift, :b_shift, :c_shift, :d_shift,
                 :key, :a_key, :b_key, :c_key, :d_key
-
 
   def initialize(key, date)
     @key = key
@@ -24,22 +22,13 @@ class Shifts
     @d_shift = @d_offset + @d_key
   end
 
-  def valid_key?
-    (key != "-00000") && (a_key[1] == b_key[0]) && (b_key[1] == c_key[0]) && (c_key[1] == d_key[0])
-  end
-
   def get_valid_key
-    possible_keys = (0...99999).to_a
-    a1 = a_key.to_i; a2 = a1 + 27; a3 = a2 + 27; a4 = a3 + 27; a5 = a4 + 27
-    b1 = b_key.to_i; b2 = b1 + 27; b3 = b2 + 27; b4 = b3 + 27; b5 = b4 + 27
-    c1 = c_key.to_i; c2 = c1 + 27; c3 = c2 + 27; c4 = c3 + 27; c5 = c4 + 27
-    d1 = d_key.to_i; d2 = d1 + 27; d3 = d2 + 27; d4 = d3 + 27; d5 = d4 + 27
+    a1 = a_key.to_i; a2 = a1 + 27; a3 = a2 + 27; a4 = a3 + 27
+    b1 = b_key.to_i; b2 = b1 + 27; b3 = b2 + 27; b4 = b3 + 27
+    c1 = c_key.to_i; c2 = c1 + 27; c3 = c2 + 27; c4 = c3 + 27
+    d1 = d_key.to_i; d2 = d1 + 27; d3 = d2 + 27; d4 = d3 + 27
     a_keys = [a1, a2, a3, a4]; b_keys = [b1, b2, b3, b4]
     c_keys = [c1, c2, c3, c4]; d_keys = [d1, d2, d3, d4]
-    potential_keys = {a_keys: [a1, a2, a3, a4],
-                      b_keys: [b1, b2, b3, b4],
-                      c_keys: [c1, c2, c3, c4],
-                      d_keys: [d1, d2, d3, d4]}
     d_keys.each do |d_key|
       c_keys.each do |c_key|
         b_keys.each do |b_key|
@@ -65,7 +54,4 @@ class Shifts
     end
     @key = "#{@a_key.to_s.rjust(2, "0")}#{@b_key.to_s.rjust(2, "0")[1]}#{@c_key.to_s.rjust(2, "0")[1]}#{d_key.to_s.rjust(2, "0")[1]}"
   end
-
-
-
 end
